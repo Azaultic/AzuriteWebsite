f00439535afedc29469a9be547175d8f93500048
Azaultic_ <azuritebe@gmail.com>
2026-01-04 14:09:45 +0100

    config default language

--- azurite-docs/docusaurus.config.js
+++ azurite-docs/docusaurus.config.js
@@ -20,25 +20,25 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Azaultic.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/AzuriteWebsite/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Azaultic', // Usually your GitHub org/user name.
+  projectName: 'AzuriteWebsite', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'nl',
-    locales: ['nl', 'nl'],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
@@ -46,13 +46,13 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        // docs: {
+        //   sidebarPath: './sidebars.js',
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   editUrl:
+        //     'https://github.com/Azaultic/AzuriteWebsite/tree/main/',
+        // },
         blog: false,
         theme: {
           customCss: './src/css/custom.css',
